131f907052b13637291e68cc11b03c42004977bd
Rhyan Vargas <rhyan@rhyanvargas.com>
2017-12-28 14:44:03 -0500

    add purify and minify output css

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,61 @@
 var webpack = require('webpack'),
     path = require('path'),
-    extractTextPlugin = require('extract-text-webpack-plugin');
+    extractTextPlugin = require('extract-text-webpack-plugin'),
+    purify = require('purifycss-webpack-plugin');
 
 module.exports = {
-    entry: './src/main.js',
+    entry: './src/index.js',
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js'
     },
-    // module: {
-    //     loaders: [{
-    //             test: /\.scss$/,
-    //             loader: extractTextPlugin.extract('css-loader!sass-loader')
-    //         }
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        watchContentBase: true,
+        port: 9000,
+        compress: true
 
-    //     ]
-
-    // },
+    },
     // Finds .scss files from path...then bundles in "bundle.css" into /dist/css folder
     module: {
         rules: [{
             test: /.scss$/,
             use: extractTextPlugin.extract({
-                use: [
-                    "css-loader",
-                    "sass-loader?sourceMap"
-                ],
-                publicPath: "/dist/css/"
+                use: [{
+                        loader: 'css-loader',
+                        options: {
+                            // If you are having trouble with urls not resolving add this setting.
+                            // See https://github.com/webpack-contrib/css-loader#url
+                            url: false,
+                            minimize: true,
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
             })
         }]
     },
     plugins: [
         new extractTextPlugin({
-            filename: "bundle.css",
+            filename: "./css/bundle.css",
             disable: false,
             allChunks: true
+        }),
+        new purify({
+            basePath: __dirname,
+            paths: [
+                './dist/*.html'
+            ],
+            purifyOptions: {
+                minify: true
+            }
         })
     ]
 }
